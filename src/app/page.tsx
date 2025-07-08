65308702ce10571097d47f0e403b96f9df03e771
Yogeesh S <yogeesh.sryd@gmail.com>
2025-07-08 19:49:50 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <main>
+        <h1>Welcome to Next.js</h1>
+      </main>
+    </div>
+  );
+}
