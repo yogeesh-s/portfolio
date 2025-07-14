1b2546fcecff68edb463ddabc9545442e5ca85d6
Yogeesh S <yogeesh.sryd@gmail.com>
2025-07-14 23:10:29 +0530

    Inital commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.unsplash.com', 'source.unsplash.com'],
+  },
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Experience from "@/components/Experience";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <main>
-        <h1>Welcome to Next.js</h1>
-      </main>
-    </div>
+    <main>
+      <Hero />
+      <About/>
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
+    </main>
   );
 }
